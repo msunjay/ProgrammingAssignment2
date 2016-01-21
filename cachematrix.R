929f184a20eb07b1d0908fbca235147409420c5c
msunjay <m_sunjay@yahoo.com>
2016-01-20 22:28:48 -0500

    completed

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,10 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      i = NULL
+      setinv <- function(x) {
+            i <<- solve(x)
+      }
 }
 
 
